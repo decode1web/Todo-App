989fa9d1617451bfc4509ce9b19952e2a06f62b0
Firdavs Gadoev <54993849+decode1web@users.noreply.github.com>
2025-01-30 22:28:18 +0300

    Update store.js

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,16 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import modalOpen from './slices/createModalOpen'
-import modalInProgress from './slices/createModalInProgress'
-import modalDone from './slices/createModalDone'
 import tasksReducer from "./slices/tasksSlice"
 import modalTaskReducer from './slices/modalTaskSlice'
 
 
 const store = configureStore({
     reducer: {
-        modalOpen,
-        modalInProgress,
-        modalDone,
         tasks: tasksReducer,
         modalTask: modalTaskReducer
     },
